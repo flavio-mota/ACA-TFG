5af45c3e4c2f43e4210e9ddbe9562d65664e0a78
flaviomota <flavio.belizario.mota@gmail.com>
2019-05-01 22:57:27 -0300

    Algoritmos parciais de treinamento

--- classificacao/ACA-C.py
+++ classificacao/ACA-C.py
@@ -8,6 +8,8 @@ Created on Wed Apr  23 16:14:34 2019
 
 import random
 import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 def constroi_automato(n, k, estados, classes):
     """
@@ -57,8 +59,77 @@ def acoes(A, n, k, amostra, ruido):
 def escolher_acao(estado, amostra, ruido):
     return 1 if np.dot(estado,amostra) + ruido > 0 else -1
 
-A, classes_ = constroi_automato(3, 5, 4, ['setosa', 'versicolor', 'virginica'])
-inicializa_estados(A, 3, 5, 0.1, 7.9, 4)
+def atualiza_estado(A, reforco, acao, eta, b, amostra, i, j):
+    if(reforco == 1):
+        A[i][j] = A[i][j] + eta*(np.dot(A[i][j],amostra) - (reforco*acao))*amostra
+    if(reforco == -1):
+        A[i][j] = A[i][j] + b*eta*(np.dot(A[i][j],amostra) - (reforco*acao))*amostra
+        
+def vizinhanca(acoes_, i, j, raio, k):
+    l = 1
+    vizinhos_ = []
+    while(l<=raio and j-l>=0):
+        vizinhos_.append(acoes_[i][j-l])
+        l+=1
+    l = 1
+    while(l<=raio and j+l<=k-1):
+        vizinhos_.append(acoes_[i][j+l])
+        l+=1
+    return vizinhos_
 
-print(acoes(A, 3, 5, [5.1, 3.5, 1.4, 0.2], 0))
+def treinamento(A, n, k, dados_treinamento, ruido, raio, eta, b):
+    dados = dados_treinamento['dados']
+    objetivos = dados_treinamento['objetivo']
+    for amostra, objetivo in zip(dados, objetivos):
+        classe_X = objetivo 
+        acoes_ = acoes(A, n, k, amostra, ruido)
+        for i in range(n):
+            for j in range(k):
+                if(classes_[i] == classe_X):
+                    if(acoes_[i][j] == 1):
+                        vizinhos_ = vizinhanca(acoes_, i, j, raio, k)
+                        if(vizinhos_.count(1) >= (len(vizinhos_)/2)):
+                            atualiza_estado(A, 1, 1, eta, b, amostra, i, j)
+                        else:
+                            atualiza_estado(A, -1, 1, eta, b, amostra, i, j)
+                    else:
+                        atualiza_estado(A, -1, -1, eta, b, amostra, i, j)
+                else:
+                    if(acoes_[i][j]==-1):
+                        vizinhos_ = vizinhanca(acoes_, i, j, raio, k)
+                        if(vizinhos_.count(-1) >= (len(vizinhos_)/2)):
+                            atualiza_estado(A, 1, -1, eta, b, amostra, i, j)
+                        else:
+                            atualiza_estado(A, -1, -1, eta, b, amostra, i, j)
+                    else:
+                        atualiza_estado(A, -1, 1, eta, b, amostra, i, j)
+
+def teste(A, n, k, dados_teste, ruido):
+    dados = dados_teste['dados']
+    objetivos = dados_teste['objetivo']
+    for amostra, objetivo in zip(dados,objetivos):
+        acoes_ = acoes(A, n, k, amostra, ruido)
+        for i in range(n):
+            if(i==objetivo):
+                
+
+
+A, classes_ = constroi_automato(3, 20, 4, [0,1,2])
+inicializa_estados(A, 3, 20, 0.1, 7.9, 4)
+print(A)
+
+iris = datasets.load_iris() #carrega o dataset IRIS
+dados = iris.data #extrai os valores de cada atributo
+y = iris.target
+
+
+X_train, X_test, y_train, y_test = train_test_split(dados, y, 
+                                    test_size = 0.3, random_state = 1, stratify=y)
+
+dados_treinamento = {'dados': X_train, 'objetivo': y_train}
+
+treinamento(A,3,20,dados_treinamento, 0.2, 5, 1, 1)
+
+
+
     
