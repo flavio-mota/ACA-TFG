0e3232c8fb9bf0bcc97a40614e93d31197af3e0f
flaviomota <flavio.belizario.mota@gmail.com>
2019-05-26 23:32:57 -0300

    Melhoria do código de agrupamentos e descontinuação do código de classificação

--- agrupamento/ACA.py
+++ agrupamento/ACA.py
@@ -13,29 +13,32 @@ def constroi_automato(n, k, estados):
     """
     Função que gera a estrutura do autômato
     
-    Parametros
+    Parâmetros
         ----------
         n : inteiro que define a quantidade de linhas da grade.
         k : inteiro que define a quantidade de colunas da grade.    
-        estados: define quantos valores compõem o estado de cada célula.
+        estados : inteiro que define quantos valores compõem o estado de uma célula.
+    
+    Retorno
+        ----------
+        automato : estrutura do autômato.
     """
     automato = [[[None for j in range(estados)]for j in range(k)]for i in range(n)]
     return automato
 
 def inicializa_estados(A, n, k, v_min, v_max, estados):
     """
-    Função que gera os valores dos estados de cada célula do autômato
-    através de uma distribução aleatória do valor mínimo e máximo do conjunto 
-    de dados
+    Função que inicializa os valores dos estados de cada célula do autômato
+    através de uma distribução aleatória de um valor mínimo e máximo
     
-    Parametros
+    Parâmetros
         ----------
-        A : Autômato Celular de xn. 
+        A : Autômato Celular de nk. 
         n : inteiro que define a quantidade de linhas da grade.
         k : inteiro que define a quantidade de colunas da grade.
-        v_min : valor mínimo do conjunto de dados.
-        v_max : valor máximo do conjunto de dados.
-        estados : quantidade de valores do estado da célula do autômato
+        v_min : inteiro/decimal que define o valor mínimo.
+        v_max : inteiro/decimal que define o valor máximo.
+        estados : inteiro que define quantos valores compõem o estado de uma célula.
     """
     for i in range(n):
         for j in range(k):
@@ -43,6 +46,24 @@ def inicializa_estados(A, n, k, v_min, v_max, estados):
             A[i][j] = estado    
 
 def obter_vizinhanca(A, i, j, amostra, tipo, ruido, raio):
+    """
+    Função que obtém as ações dos vizinhos de uma determinada célula.
+    
+    Parâmetros
+        ----------
+        A : Autômato Celular de nk.
+        i : inteiro que define a posição da célula na linha.
+        j : inteiro que define a posição da célula na grade.    
+        amostra : vetor com o dados apresentados às células.
+        tipo : inteiro que determina qual o tipo de vizinhança a ser utilizada,
+                podendo ser Moore (1) ou Von Neumann(2).
+        ruido : decimal que é utilizado no cálculo da ação.
+        raio : inteiro que determina quantos níveis de vizinhos devem ser visitados.
+    
+    Retorno
+        ----------
+        vizinhos_ : vetor contendo as ações dos vizinhos de uma determinada célula.
+    """
     if(tipo == 1): #vizinhança de Moore
         vizinhos_ = []
         for m in range(i-raio,i+raio+1):
@@ -51,7 +72,7 @@ def obter_vizinhanca(A, i, j, amostra, tipo, ruido, raio):
                     if(m != i or n != j):
                         vizinhos_.append(escolher_acao(A[m][n],amostra,ruido))
         return vizinhos_
-    else: #vizinhança de Von Neumann
+    if(tipo == 2): #vizinhança de Von Neumann
         vizinhos_ = []
         for m in range(i-raio,i+raio+1):
             for n in range(j-raio,j+raio+1):
@@ -66,12 +87,147 @@ def obter_vizinhanca(A, i, j, amostra, tipo, ruido, raio):
                         
 
 def escolher_acao(estado, amostra, ruido):
+    """
+    Função que determina a ação de uma célula.
+    
+    Parâmetros
+        ----------
+        estado : vetor com o estado atual da célula.
+        amostra : vetor com o dados apresentados à célula.
+        ruido : decimal que é utilizado no cálculo da ação.
+    
+    Retorno
+        ----------
+        Distância euclidiana entre dois pontos somada ao ruído.
+    """
     return distance.euclidean(estado, amostra)+ruido
 
 def reforco(A, i, j, amostra, eta):
+    """
+    Função que aplica o reforço em uma determinada célula, atualizando seu estado.
+    
+    Parâmetros
+        ----------
+        A : Autômato Celular de nk.
+        i : inteiro que define a posição da célula na linha.
+        j : inteiro que define a posição da célula na grade.    
+        amostra : vetor com o dados apresentados às células.
+        eta : decimal que determina a taxa de aprendizagem.
+    
+    """
     A[i][j] = A[i][j] + (eta*((amostra) - (A[i][j])))
+    
+def centroides(A, n, k, grupos):
+    """
+    Função que determina os protótipos dos centróides do agrupamento.
+    
+    Parâmetros
+        ----------
+        A : Autômato Celular de nk.
+        n : inteiro que define a quantidade de linhas da grade.
+        k : inteiro que define a quantidade de colunas da grade.    
+        grupos : matriz contendo um indicador de que a célula mudou de estado
+                 durante o processo de aprendizagem, sinalizado um centro.
+    
+    Retorno
+        ----------
+        Matriz contendo os centróides gerados pelo agrupamento.
+    """
+    centroides = []
+    for i in range(n):
+        for j in range(k):
+            if(grupos[i][j]==1):
+                centroides.append(A[i][j])
+    
+    
+    cent = np.unique(np.array(centroides),axis=0)
 
-def agrupar(A, l, c, dados, ruido, eta, probabilidades, grupos, n_iter, raio, max_iter):
+#TODO analisar a implementação de um 'merge' dos centróides próximos    
+#    cent_ = []
+#    for atual in cent:
+#        prox_ = []
+#        for centro in cent:
+#            if(distance.euclidean(atual, centro) < 1.0):
+#                prox_.append(centro)
+#        if(len(prox_)>0):        
+#            estado = np.mean(prox_, axis=0)
+#            cent_.append(estado)
+#        else:
+#            cent_.append(atual)
+        
+    return np.asarray(cent)
+
+def rotular(A, n, k, amostras, objetivos, centroides_):
+    """
+    Função que determina o rótulo de cada grupo e de cada amostra de dado.
+    
+    Parâmetros
+        ----------
+        A : Autômato Celular de nk.
+        n : inteiro que define a quantidade de linhas da grade.
+        k : inteiro que define a quantidade de colunas da grade.    
+        amostras : matriz contendo o conjunto de dados agrupados.
+        objetivos : vetor contendo o rótulo real de cada dado.
+        centroides_ : vetor contendo os centróides gerados.
+    
+    Retorno
+        ----------
+        rotulos : dicionário contendo os rótulos de cada grupo.
+        y_aca : vetor contendo os rótulos gerados para cada dado.
+    """
+    rotulos = {}
+    for i in range(len(centroides_)):
+        rotulos[i] = 0
+    
+    i = 0
+    for centro in centroides_:
+        distancias = []
+        for dado in amostras:
+            distancias.append(distance.euclidean(centro, dado))
+        rotulo = objetivos[distancias.index(min(distancias))]
+        rotulos[i] = rotulo
+        i+=1
+    
+    y_aca = []
+    
+    for dado in amostras:
+        min_dist = distance.euclidean(centroides_[0], dado)
+        i, ind = 0, 0
+        for centro in centroides_:
+            if(distance.euclidean(centro, dado) < min_dist):
+                min_dist = distance.euclidean(centro, dado)
+                ind = i
+            i+=1
+        y_aca.append(rotulos[ind])
+                
+    return rotulos, y_aca
+
+def agrupar(A, l, c, dados, objetivos, ruido, eta, probabilidades, n_iter, raio, max_iter):
+    """
+    Função que realiza o agrupamento.
+    
+    Parâmetros
+        ----------
+        A : Autômato Celular de nk.
+        l : inteiro que define a quantidade de linhas da grade.
+        c : inteiro que define a quantidade de colunas da grade.    
+        dados : matriz contendo o conjunto de dados a serem agrupados.
+        objetivos : vetor contendo o rótulo real de cada dado.
+        ruido : decimal que é utilizado no cálculo da ação.
+        eta : decimal que determina a taxa de aprendizagem.
+        probabilidades : estrutura de vetor para armazenar as probabilidades 
+                         de um dado rotulado pertencer a uma célula.
+        n_iter : inteiro que define o número máximo de iterações do processo de agrupamento.
+        aio : inteiro que determina quantos níveis de vizinhos devem ser visitados.
+        max_iter : inteiro que determina o número máximo de iterações onde os estados não mudam (estabilidade).
+    
+    Retorno
+        ----------
+        centroides_ : matriz contendo os centróides gerados pelo agrupamento.
+        rotulos : dicionário contendo os rótulos de cada grupo.
+        y_aca : vetor contendo os rótulos gerados para cada dado.
+    """
+    grupos = constroi_automato(l,c,1)
     A_ant = np.copy(A)
     convergence = True
     epocas = 0
@@ -91,6 +247,7 @@ def agrupar(A, l, c, dados, ruido, eta, probabilidades, grupos, n_iter, raio, ma
                         v_prob[x] = 100 - acoes[0]
                     x +=1
             probabilidades[a] = v_prob
+        
         mudou = False
         for i in range(l):
             for j in range(c):
@@ -111,8 +268,29 @@ def agrupar(A, l, c, dados, ruido, eta, probabilidades, grupos, n_iter, raio, ma
         print (grupos[i])
     print(" ")
     print(epocas)
+    
+    centroides_ = centroides(A,l,c,grupos)
 
+    rotulos, y_aca = rotular(A,l,c,dados,objetivos,centroides_)
+    
+    return centroides_, rotulos, y_aca
+
+#TODO analisar a necessidade dessa função (remover!)
 def mostrar_classes(objetivo, grupos, probabilidades, l, c):
+    """
+    Função que mostra o rótulo dos grupos criados para dados rotulados.
+    
+    Parâmetros
+        ----------
+        objetivo : vetor contendo o rótulo real de cada dado.
+        grupos : matriz contendo o sinalizador de que uma célula participou do
+                 processo de agrupamento.
+        probabilidades : vetor que armazena a probabilidade de um dado rotulado 
+                         pertencer a uma célula.
+        l : inteiro que define a quantidade de linhas da grade.
+        c : inteiro que define a quantidade de colunas da grade.
+        
+    """    
     #Seleciona os rótulos que existem no conjunto e cria um dicionario pra eles
     rotulo = np.unique(objetivo)
     rotulos = {}
@@ -138,15 +316,6 @@ def mostrar_classes(objetivo, grupos, probabilidades, l, c):
     print(" ")
     
 
-def centroides(A, n, k, grupos):
-    centroides = []
-    for i in range(n):
-        for j in range(k):
-            if(grupos[i][j]==1):
-                centroides.append(A[i][j])
-    
-    cent = np.unique(np.array(centroides),axis=0)
-    return np.asarray(cent)
 
 
 #def silhueta(dados, centroides, objetivos):
--- agrupamento/agrupamento.py
+++ agrupamento/agrupamento.py
@@ -8,33 +8,36 @@ Created on Tue Feb  22 20:41:47 2019
 import ACA as ac
 from sklearn import datasets
 import matplotlib.pyplot as plt
-
+from sklearn.metrics import cluster
 
 
 """
-----------PARAMÊTROS
+----------PARÂMETROS
 """
 #iris = datasets.load_iris() #carrega o dataset IRIS
 #dados = iris.data #extrai os valores de cada atributo
 #objetivo = iris.target
 #
-X, y = datasets.make_blobs(n_samples=150, n_features=2, centers=3, cluster_std=0.5, 
-                  shuffle=True, random_state=0)
-#
+#X, y = datasets.make_blobs(n_samples=150, n_features=2, centers=3, cluster_std=0.5,
+#                           shuffle=True, random_state=0)
+
 #X, y = datasets.make_moons(n_samples=200,
 #                           noise=.05,
 #                           random_state=0)
 
-l = 4 #define quantidade de linhas
-c = 3 #define quantidade de colunas    
+X, y=  datasets.make_circles(n_samples=700, factor=.5,
+                                      noise=.05)
+
+l = 5 #define quantidade de linhas
+c = 5 #define quantidade de colunas    
 estados = 2 #extrai quantos valores de estados serão necessários
 ruido = 0.1 #ruído da função de escolha de ação
 eta = 0.1 #taxa de aprendizagem
 v_min = -1 #valor mínimo do conjunto de dados
 v_max = 1 #valor máximo do conjunto de dado
 n_iter = 100 #número de iterações
-raio = 1 #vizinhança que será analisada
-max_iter = 5 #número máximo para convergência
+raio = 2 #vizinhança que será analisada
+max_iter = 15 #número máximo para convergência
 """
 ------------------------------
 """
@@ -45,19 +48,22 @@ A = ac.constroi_automato(l,c,estados)
 #inicializar os estados
 ac.inicializa_estados(A, l, c, v_min, v_max, estados) 
 
-#matriz que verifica quais células mudaram de estado
-grupos = ac.constroi_automato(l,c,1)
-
 #Vetor de probabilidade
 probabilidades = [' ' for _ in range(len(y))]
 
 #APRENDIZAGEM DO AUTÔMATO
-ac.agrupar(A, l, c, X, ruido, eta, probabilidades, grupos, n_iter, raio, max_iter)
+centroides, rotulos, y_aca = ac.agrupar(A, l, c, X, y, ruido, eta, probabilidades, n_iter, raio, max_iter)
 
-centroides = ac.centroides(A,l,c,grupos)
+print('Rand: ', cluster.adjusted_rand_score(y, y_aca))
 
-plt.scatter(X[:,0],X[:,1],c='white', marker='o',edgecolor='black',s=50)
+plt.scatter(X[y == 0, 0],X[y == 0, 1],c='blue', marker='o',edgecolor='black',s=50)
+plt.scatter(X[y == 1, 0],X[y == 1, 1],c='green', marker='o',edgecolor='black',s=50)
+plt.scatter(X[y == 2, 0],X[y == 2, 1],c='orange', marker='o',edgecolor='black',s=50)
+#plt.scatter(X[y == 3, 0],X[y == 3, 1],c='yellow', marker='o',edgecolor='black',s=50)
+#plt.scatter(X[y == 4, 0],X[y == 4, 1],c='black', marker='o',edgecolor='black',s=50)
+#plt.scatter(X[y == 5, 0],X[y == 5, 1],c='pink', marker='o',edgecolor='black',s=50)
 plt.scatter(centroides[:,0],centroides[:,1],c='red', marker='*',edgecolor='black',s=250)
+
 plt.grid()
 plt.show()
 #ac.mostrar_classes(objetivo, grupos, probabilidades, l, c)
